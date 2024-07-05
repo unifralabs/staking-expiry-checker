30857ecf2b638cc8390f3164f8cb1eb2db7365de
vincent <vincent@unifra.io>
2024-07-05 15:59:21 +0800

    Revert "chore: update btc client http mode"
    This reverts commit fec7393485af29360fa6b4ebf244dbc4d7615487.

--- internal/btcclient/btcclient.go
+++ internal/btcclient/btcclient.go
@@ -21,7 +21,7 @@ func NewBtcClient(cfg *config.BtcConfig) (*BtcClient, error) {
 
 	connCfg := &rpcclient.ConnConfig{
 		Host:         cfg.Endpoint,
-		HTTPPostMode: false,
+		HTTPPostMode: true,
 		User:         cfg.RpcUser,
 		Pass:         cfg.RpcPass,
 		DisableTLS:   cfg.DisableTLS,
